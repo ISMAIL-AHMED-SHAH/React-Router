980a45646a09b005fcd47d7a390b0dde7f571173
Ismail Ahmed <aismail@kictl.com>
2024-08-22 10:04:06 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Layout from './Layout.jsx'
-import Home from '../components/Home/Home.jsx'
+import Home from './Components/Home/Home.jsx'
+
 import About from './Components/About/About.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 import User from './Components/User/User.jsx'
 import Github, { githubInfoLoader } from './Components/Github/Github.jsx'
 
-
-
 import './index.css'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
+import Header from './Components/Header/Header.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -38,6 +38,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
+      <Route path='' element={<Header />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
